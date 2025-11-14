1319550c0a197b2454260fd7bcdd90373cb69f6f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-14 12:25:15 +0700

    chore(comps): add typography component

---
+++ src/components/typography.tsx
@@ -0,0 +1,51 @@
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+
+type TypographyProps = {
+  children: React.ReactNode
+  className?: string
+}
+
+type LinkTypographyProps = TypographyProps & {
+  href: string
+  target?: '_blank' | '_self'
+}
+
+export const H1 = ({ children, className }: TypographyProps) => {
+  return (
+    <h1
+      className={cn('text-pretty md:text-[64px] text-blue-11 md:leading-16 font-inter', className)}
+    >
+      {children}
+    </h1>
+  )
+}
+
+export const P = ({ children, className }: TypographyProps) => {
+  return (
+    <p
+      className={cn(
+        'text-pretty text-lg leading-7 tracking-normal text-blue-11 font-inter',
+        className,
+      )}
+    >
+      {children}
+    </p>
+  )
+}
+
+export const LinkP = ({ children, className, href, target = '_blank' }: LinkTypographyProps) => {
+  return (
+    <Link
+      href={href}
+      target={target}
+      className={cn(
+        'text-pretty text-lg leading-7 tracking-normal text-blue-11 font-inter underline',
+        className,
+      )}
+      rel="noopener noreferrer"
+    >
+      {children}
+    </Link>
+  )
+}
