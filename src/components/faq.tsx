995ff84f2aae318a6629b4ecfee2a61712160098
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-20 21:33:55 +0700

    feat(faq): enhance FAQ section with introductory text and improved layout

--- src/components/faq.tsx
+++ src/components/faq.tsx
@@ -1,5 +1,5 @@
 import { FAQ_ITEMS } from '@/data/faq-items'
-import { H2 } from './typography'
+import { H2, P } from './typography'
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './ui/accordion'
 import { useId } from 'react'
 
@@ -9,9 +9,14 @@ const FAQ = () => {
   return (
     <section
       id="faq"
-      className="relative p-8 md:p-16 w-full md:w-2xl h-fit flex flex-col items-center justify-center overflow-hidden"
+      className="relative p-8 md:p-16 w-full md:w-2xl h-fit flex flex-col items-center justify-center overflow-hidden gap-4"
     >
-      <H2>FAQs</H2>
+      <H2>Got questions? We&apos;ve got you.</H2>
+      <P className="text-center text-sm">
+        Find clear, friendly answers to help you get the most out of Trusynk. No pressure, no
+        confusion — just simple guidance to keep your networking flow smooth. And if you ever need
+        more help, we&apos;re always here for you.
+      </P>
       <Accordion type="single" collapsible className="w-full">
         {FAQ_ITEMS.slice(0, 7).map((item, index) => (
           <AccordionItem key={`${id}-${index}`} value={`${id}-${index}`}>
