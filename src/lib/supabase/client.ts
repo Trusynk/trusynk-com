faadde2e5c036ae39468baf28cd3a3025d1e37e0
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-11-22 14:51:31 +0700

    chore: move supabase folder to lib folder

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,4 +1,4 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -7,4 +7,4 @@ if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables')
 }
 
-export const createClient = () => createBrowserClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, { db: { schema: 'dtwts' } })
